9e136555f35924a13671bec01c10800619041eb4
“Hege <hege@checkin.no>
2023-03-09 11:28:51 +0100

    Start av test

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {PostForm} from './components/PostForm'
+import NAMES from './components/data.json'
+import { useState } from 'react'
+
+interface data {
+  id: number
+  firstName: string
+  lastName: string
+}
 
 function App() {
+  const [query, setQuery] = useState('')
+  const changeHandler = (event:any) => {
+    setQuery(event.target.value)
+  }
+  const filteredNames = NAMES.filter(item => {
+    return item.firstName.includes(query) || item.lastName.includes(query)
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <PostForm/>
+        <input type='text' value={query} onChange={changeHandler}></input>
+        {
+          filteredNames.map((item) => (
+            <p key={item.id}> {item.firstName} {item.lastName} </p>
+          ))
+        }
+      </div>
     </div>
   );
 }
